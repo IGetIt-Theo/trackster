25ceb5996370685bb7e2cb15cbdb277b89338443
TheoIGetIt <theo@igetit.net>
2017-04-26 15:18:19 -0700

    Adding API calls

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,7 +1,7 @@
 var Trackster = {};
 
 $(document).ready(function() {
-  $('search-button').click(function() {
+  $('#search-button').click(function() {
     Trackster.searchTracksByTitle($('#search-input').val());
   });
 });
@@ -19,5 +19,6 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-  alert('searchTracksByTitle' + title);
+  console.log( $.ajax("https://api.spotify.com/v1/search?type=track&q="+title));
+  
 };
