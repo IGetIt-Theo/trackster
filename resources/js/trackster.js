cbc9c34cb9e73a490ab99a266120a0076f91bd3c
TheoIGetIt <theo@igetit.net>
2017-04-29 13:32:31 -0700

    Tracks added

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -11,7 +11,15 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-  alert('renderTracks');
+  for (idx=0; idx<tracks.lenght; idx++){
+    var trackHtml='<div class="row song-row">' +
+      '<div class="col-lg-1 col-xs-1"> <a href="https://p.scdn.co/mp3-preview/22bf10aff02db272f0a053dff5c0063d729df988?cid=null"><i class="fa fa-play-circle-o fa-2x"></i></a></div> ' +
+      '<div class="col-lg-4 col-xs-4">Stairway To Heaven</div>' +
+      '<div class="col-lg-3 col-xs-3">Led Zeppelin</div>' +
+      '<div class="col-lg-2 col-xs-2">Led Zeppelin IV (Deluxe Edition)</div>' +
+      '<div class="col-lg-2 col-xs-2">76</div> </div>';
+    $('#song-list').append(trackHtml);
+  }
 };
 
 /*
@@ -19,6 +27,6 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-  console.log( $.ajax("https://api.spotify.com/v1/search?type=track&q="+title));
-  
+  /*console.log( $.ajax("https://api.spotify.com/v1/search?type=track&q="+title));*/
+  renderTracks($.ajax("https://api.spotify.com/v1/search?type=track&q="+title));
 };
